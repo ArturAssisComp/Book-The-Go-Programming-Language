5d65a25d711c1a11db11054a18635bafae2b6840
ArturAssisComp <arturassiscomp@gmail.com>
2022-07-21 17:04:31 -0300

    Implement print args program

---
+++ chap1_Tutorial/print_args.go
@@ -0,0 +1,28 @@
+/*
+Name of the program: print_args
+Topics covered:
+    - Variable short declaration;
+    - String concatenation;
+    - Loops;
+    - Importing multiple libs;
+    - Basic slicing;
+    - Int to string convertion;
+*/
+
+//Prints the arguments from CLI.
+package main 
+
+import (
+    "fmt"
+    "os"
+    "strconv"
+)
+
+
+func main() {
+    fmt.Println("    Command: " + os.Args[0]);
+    for index, argument := range os.Args[1:] { //:= is used for short declaration.
+        fmt.Println("Argument[" + strconv.Itoa(index + 1) + "]: " + argument)
+    }
+
+}
